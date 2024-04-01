11386e7c75d08a928f385941bdb708a64c1ee99c
Matias <mati.delarosa1984@gmail.com>
2024-04-01 12:19:40 -0300

    router setup

--- e-commerce/api/src/App.jsx
+++ e-commerce/api/src/App.jsx
@@ -1,8 +1,25 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
+import { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private } from './pages'
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Sidebar/>
+        <Routes>
+           <Route path='/' element={<Home/>}/>
+           <Route path='/products' element={<Products/>}/>
+           <Route path='/singleProduct/:id' element={<SingleProduct/>}/>
+           <Route path='/about' element={<About/>}/>
+           <Route path='/cart' element={<Cart/>}/>
+           <Route path='/Error' element={<Error/>}/>
+           <Route path='/checkout' element={<Checkout/>}/>
+           <Route path='/private' element={<Private/>}/>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
+  ) 
 }
 
 export default App
