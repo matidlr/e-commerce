f1778bcf374580e53f5e2a7a74e90b978e423dfc
Matias <mati.delarosa1984@gmail.com>
2024-04-02 13:14:00 -0300

    about and checkout page

--- e-commerce/api/src/pages/AboutPage.jsx
+++ e-commerce/api/src/pages/AboutPage.jsx
@@ -4,7 +4,28 @@ import { PageHero } from '../components'
 import aboutImg from '../assets/hero-bcg.jpeg'
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return <main>
+      <PageHero title="about"/>
+      <Wrapper className='page section section-center'>
+          <img src={aboutImg} alt="nice desk" />
+          <article>
+            <div className="title">
+                <h2>our story</h2>
+            </div>
+            <p>
+
+                A FEW OF THE REASONS YOU SHOULD BUY FROM US:
+                We use national fulfillment centers to ship your orders. We have partnered with Amazon.com for all order fulfillment and have our products strategically placed in warehouses across the United States to ensure fast and reliable order shipping.
+                It’s our goal to offer you a customer-centered shopping experience.
+                Personal, knowledgeable and professional support
+                All our products has been through a thorough field test in the Nordic countries, before making it available to our customers.
+                Secure Ordering available
+                100% Satisfaction Money back guarantee
+                It's
+            </p>
+          </article>
+      </Wrapper>
+  </main>
 }
 
 const Wrapper = styled.section`
