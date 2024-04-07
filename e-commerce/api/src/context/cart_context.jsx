e7aab742142dd876083e8561667433fb24f076ed
Matias <mati.delarosa1984@gmail.com>
2024-04-07 16:19:38 -0300

    remove item and clear cart

--- e-commerce/api/src/context/cart_context.jsx
+++ e-commerce/api/src/context/cart_context.jsx
@@ -38,12 +38,16 @@ const addToCart = (id, color, amount, product) => {
 
 // remove item
 const removeItem = (id) => {
-
+   dispatch({ type: REMOVE_CART_ITEM, payload: id })
 }
 // toggle amount
-const toggleAmount = (id, value) {}
+const toggleAmount = (id, value) {
+
+}
 // clear cart
-const clearCart = () =>{}
+const clearCart = () =>{
+  dispatch({type: CLEAR_CART})
+}
 
 useEffect(()=>{
   localStorage.setItem('cart', JSON.stringify(state.cart))
