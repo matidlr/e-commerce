627031c6a8beb584c62e7649ee1a6724b9e3bb51
Matias <mati.delarosa1984@gmail.com>
2024-04-08 10:29:39 -0300

    private route

--- e-commerce/api/src/App.jsx
+++ e-commerce/api/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Navbar, Sidebar, Footer } from './components';
 import { Home, Products, SingleProduct, About, Cart, Error, Checkout, Private } from './pages'
+import PrivateRoute from './pages/PrivateRoute';
 
 function App() {
   return (
@@ -9,12 +10,18 @@ function App() {
     <Sidebar/>
         <Routes>
            <Route path='/' element={<Home/>}/>
-           <Route path='/products' element={<Products/>}/>
-           <Route path='/singleProduct/:id' element={<SingleProduct/>}/>
-           <Route path='/about' element={<About/>}/>
-           <Route path='/cart' element={<Cart/>}/>
-           <Route path='/Error' element={<Error/>}/>
-           <Route path='/checkout' element={<Checkout/>}/>
+           <Route path='products' element={<Products/>}/>
+           <Route path='singleProduct/:id' element={<SingleProduct/>}/>
+           <Route path='about' element={<About/>}/>
+           <Route path='cart' element={<Cart/>}/>
+           <Route path='Error' element={<Error/>}/>
+           <Route 
+                path='/checkout' 
+                element={<PrivateRoute>
+                       <Checkout/>
+                  </PrivateRoute>
+                }/>
+          
            <Route path='/private' element={<Private/>}/>
         </Routes>
         <Footer />
