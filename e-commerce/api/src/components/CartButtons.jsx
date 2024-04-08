bafe64dce404bd6b470ea7c84d5f567256b41127
Matias <mati.delarosa1984@gmail.com>
2024-04-07 21:06:13 -0300

    auth0-toggle values

--- e-commerce/api/src/components/CartButtons.jsx
+++ e-commerce/api/src/components/CartButtons.jsx
@@ -19,12 +19,15 @@ const CartButtons = () => {
                 <span className='cart-value'>{total_items}</span>
             </span>
         </Link>
-        <button type='button' className='auth-btn' onClick={loginWithRedirect}>
+        {myUser ? (
+         <button type='button' className='auth-btn' onClick={()=> logout({returnTo:window.location.origin})}>
+         Logout <FaUserMinus />
+      </button>
+        ) : (
+         <button type='button' className='auth-btn' onClick={loginWithRedirect}>
            Login <FaUserPlus />
         </button>
-        <button type='button' className='auth-btn' onClick={()=> logout({returnTo:window.location.origin})}>
-           Logout <FaUserMinus />
-        </button>
+        )}
   </Wrapper>
 }
 
--- e-commerce/api/src/context/user_context.jsx
+++ e-commerce/api/src/context/user_context.jsx
@@ -6,6 +6,14 @@ export const UserProvider = ({ children }) => {
   const {isAuthenticated, loginWithRedirect, logout, user, isLoading} = useAuth0()
 
   const [myUser, setMyUser] = useState(null)
+  
+  useEffect(() =>{
+    if(isAuthenticated){
+      setMyUser(user)
+    } else{
+      setMyUser(false)
+    }
+  },[isAuthenticated])
 
   return (
     <UserContext.Provider value={{loginWithRedirect, logout, myUser}}>{children}</UserContext.Provider>
